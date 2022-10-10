bf5c871eb74618c5218d1034ca85c41a8930f59a
Adrian Estopace <estopaceadrian@gmail.com>
2022-10-10 14:28:58 +0800

    added model and bind data to components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeComponent } from './recipes/recipes.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    RecipeComponent,
     RecipeListComponent,
     RecipeDetailComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
+    RecipeItemComponent,
   ],
   imports: [BrowserModule],
   providers: [],
